a01fea3c067912d194e327a277372d2df21f1578
Harshagowda42 <152777615+Harshagowda42@users.noreply.github.com>
2024-12-09 18:52:01 +0530

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,14 +1,6 @@
-// function reversedString(input) {
-//     const reversed = input.toString().split('').reverse().join('');
-//     return reversed;
-// }
-
-// const n = 1225;
-// console.log(reversedString(n)); // Output: "5221"
-
 function reversedString(input) {
     const reversed = input.toString().split('').reverse().join('');
     return reversed;
 }
 const n = 12234;
-console.log(reversedString(n));
+console.log(reversedString(n)); // Output: "5221"
