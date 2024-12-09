480f8a76cc92b2a2cf2587cb017f3fafba9d20be
HarshaGowda <harshagowda4224@gmail.com>
2024-12-09 18:48:21 +0530

    0.1 Program

---
+++ 01.js
@@ -0,0 +1,14 @@
+// function reversedString(input) {
+//     const reversed = input.toString().split('').reverse().join('');
+//     return reversed;
+// }
+
+// const n = 1225;
+// console.log(reversedString(n)); // Output: "5221"
+
+function reversedString(input) {
+    const reversed = input.toString().split('').reverse().join('');
+    return reversed;
+}
+const n = 12234;
+console.log(reversedString(n));
